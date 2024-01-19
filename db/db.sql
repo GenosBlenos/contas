4372d6f9b1c3304b02857d139f65c649049609ce
Matheus Ginna <107595564+GenosBlenos@users.noreply.github.com>
2024-01-19 11:45:43 -0300

    Esqueleto Inicial
    Base inicial para o projeto de compras

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Notas (
+    ID int NOT NULL,
+    Tipo varchar(255) NOT NULL,
+    Descricao varchar(255),
+);
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include "header.html";
+
+$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+
+include "index.html";
+
+include "footer.html";
