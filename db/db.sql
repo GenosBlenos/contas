c611a997a36d220b6df37bac22a0a5fa538e995b
Matheus Ginna <107595564+GenosBlenos@users.noreply.github.com>
2024-04-23 12:03:50 -0300

    Banco de dados e paginas HTML
    Estruturação do Banco de Dados de forma básica, foram também criadas páginas HTML para a inserção do código em PHP.

--- db/db.sql
+++ db/db.sql
@@ -1,5 +1,72 @@
-CREATE TABLE Notas (
-    ID int NOT NULL,
-    Tipo varchar(255) NOT NULL,
-    Descricao varchar(255),
+CREATE TABLE 'energia' (
+    'id_energia' int not NULL primary key,
+    'mes' int (2),
+    'instalacao' int (10),
+    'vencimento' date,
+    'consumo' float not NULL,
+    'multa' float,
+    'total' float,
 );
+
+CREATE TABLE 'telefone' (
+    'id_telefone' int not NULL primary key,
+    'mes' int (2),
+    'numero' int (1),
+    'tridigito' int,
+    'vencimento' date,
+    'consumo' float not NULL,
+    'multa' float,
+    'cobrar' float,
+    'total' float,
+);
+
+CREATE TABLE 'agua' (
+    'id_agua' int NOT NULL primary key,
+    'mes' int (2),
+    'medido' int not NULL,
+    'faturado' int,
+    'tarifa' float,
+    'afastamento' float,
+    'esgoto' float,
+    'desconto' float,
+    'outros' float,
+    'vencimento' date
+    'multa' float,
+    'total' float,
+);
+
+CREATE TABLE 'estagiario' (
+    'nome' varchar not NULL primary key,
+    'unidade' varchar,
+    'pagamento' varchar,
+    'secretaria' varchar,
+    'contrato' date,
+    'mes' int (2),
+);
+
+CREATE TABLE 'semparar' (
+    'placa' varchar not NULL primary key,
+    'combustivel' varchar,
+    'veiculo' varchar,
+    'marca' varchar,
+    'modelo' varchar,
+    'tipo' varchar,
+    'departamento' varchar,
+    'ficha' int,
+    'secretaria' varchar,
+    'tag' int,
+    'mensalidade' float,
+    'passagens' float,
+    'estacionamento' float,
+    'estabelecimentos' float,
+    'credito' float,
+    'isento' boolean,
+    'mes' int (2),
+    'total' float,
+);
+
+select cast(tarifa as decimal(10,3)) from agua;
+
+select cast(afastamento as decimal(10,3)) from agua;
+
+select cast(esgoto as decimal(10,3)) from agua;
